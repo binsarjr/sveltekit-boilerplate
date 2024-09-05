26ff7d8e10ae58e1bca137d256389a2b2ff4bc3e
binsarjr <binsarjr121@gmail.com>
2024-09-05 23:12:17 +0700

    feat: create Card component suite with header, footer, and content sections for structured UI layouts and improved usability

--- src/lib/components/ui/avatar/index.ts
+++ src/lib/components/ui/avatar/index.ts
@@ -2,12 +2,11 @@ import _Root from './avatar.svelte';
 import Image from './avatar-image.svelte';
 import Fallback from './avatar-fallback.svelte';
 
-// @ts-ignore
-const Avatar: typeof _Root & {
-	Image: typeof Image;
-	Fallback: typeof Fallback;
-} = _Root;
+const Avatar: any = _Root;
 Avatar.Image = Image;
 Avatar.Fallback = Fallback;
 
-export default Avatar;
+export default Avatar as typeof _Root & {
+	Image: typeof Image;
+	Fallback: typeof Fallback;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,11 @@
 	import FormSelect from '@forms/FormSelect.svelte';
 	import FormTextInput from '@forms/FormTextInput.svelte';
 	import FormUpload from '@forms/FormUpload.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 	import { superForm } from 'sveltekit-superforms';
 	import { z } from 'zod';
 	const schema = z.object({
@@ -17,6 +22,37 @@
 </script>
 
 <div class="container">
+	<Card.Root class="w-[350px]">
+		<Card.Header>
+			<Card.Title>Create project</Card.Title>
+			<Card.Description>Deploy your new project in one-click.</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<form>
+				<div class="grid w-full items-center gap-4">
+					<div class="flex flex-col space-y-1.5">
+						<Label for="name">Name</Label>
+						<Input id="name" placeholder="Name of your project" />
+					</div>
+					<div class="flex flex-col space-y-1.5">
+						<Label for="framework">Framework</Label>
+						<Select.Root>
+							<Select.Trigger id="framework">
+								<Select.Value placeholder="Select" />
+							</Select.Trigger>
+							<Select.Content></Select.Content>
+						</Select.Root>
+					</div>
+				</div>
+			</form>
+		</Card.Content>
+		<Card.Footer class="flex justify-between">
+			<Button variant="outline">Cancel</Button>
+			<Button>Deploy</Button>
+		</Card.Footer>
+	</Card.Root>
+	Copy
+
 	<Avatar>
 		<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
 		<Avatar.Fallback>CN</Avatar.Fallback>
