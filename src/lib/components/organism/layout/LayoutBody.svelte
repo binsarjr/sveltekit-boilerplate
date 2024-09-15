71497e5ff69ee0cf619979bb83135bbf0cf79468
binsarjr <binsarjr121@gmail.com>
2024-09-15 12:51:33 +0700

    feat: add LayoutBody component for layout structure with error handling in context retrieval and responsive styling considerations

---
+++ src/lib/components/organism/layout/LayoutBody.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { getLayoutContext } from './Layout.svelte';
+	import { browser } from '$app/environment';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '@/utils';
+
+	type $$Props = HTMLAttributes<HTMLDivElement>;
+
+	const layoutContext = getLayoutContext();
+	if (!$layoutContext) {
+		throw new Error('Layout context not found');
+	}
+</script>
+
+<section
+	data-layout="body"
+	{...$$restProps}
+	class={cn(
+		'px-4 py-6 md:overflow-hidden md:px-8',
+		$layoutContext.fixed && 'flex-1',
+		$$props.class
+	)}
+>
+	<slot />
+</section>
