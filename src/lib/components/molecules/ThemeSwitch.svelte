7f9fc818251fc92b160f1b89b3952ef6c090528b
binsarjr <binsarjr121@gmail.com>
2024-09-15 13:08:01 +0700

    feat: add ThemeSwitch component for dynamic theme toggling between light and dark modes, enhancing user experience in UI

---
+++ src/lib/components/molecules/ThemeSwitch.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Button } from '@atoms/button';
+	import IconMoon from '@icons/IconMoon.svelte';
+	import IconSun from '@icons/IconSun.svelte';
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+
+	const theme = writable<'light' | 'dark'>('light');
+	function toggleTheme() {
+		$theme = $theme === 'light' ? 'dark' : 'light';
+	}
+
+	onMount(() => {
+		const updateThemeColor = () => {
+			const themeColor = $theme === 'dark' ? '#020817' : '#fff';
+			const metaThemeColor = document.querySelector("meta[name='theme-color']");
+			if (metaThemeColor) {
+				metaThemeColor.setAttribute('content', themeColor);
+			}
+		};
+
+		updateThemeColor();
+		return theme.subscribe(updateThemeColor);
+	});
+</script>
+
+<Button size="icon" variant="ghost" class="rounded-full" on:click={toggleTheme}>
+	{#if $theme === 'light'}
+		<IconMoon class="text-[20px]" />
+	{:else}
+		<IconSun class="text-[20px]" />
+	{/if}
+</Button>
